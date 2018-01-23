f742a1ceba3a7fe678124429d308e75b563c1dc4
JH_Kim <kimjh6980@naver.com>
2018-01-22 17:35:17 -0800

    Add some comment

--- 180122/example.py
+++ 180122/example.py
@@ -11,6 +11,7 @@ def my_func(**kwargs):
     print("third arg: !", kwargs.get('third_arg',-3))
     print("Fourth_arg: ", kwargs.get('fourth_arg', -4))
 
-help(my_func)
+help(my_func) # The help function
+
 my_func(first_arg = ['a', 'b', 'c'],
         second_arg={'x' : 1, 'y' : 2, 'z' : 3})
